af513219415d076bd2a8654ba7182c9757ea54dd
Edgardo Alvarez <edgardo@livedesign.org>
2023-09-02 18:55:42 -0500

    “update”

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,11 +1,12 @@
 import { Typography } from "@mui/material"
 import { MailOutline } from "@mui/icons-material"
+import { JournalLayout } from "../layout/JournalLayout"
 
 export const JournalPage = () => {
     return (
-        <>
-            <Typography variant="h1">JournalPage</Typography>
-            <MailOutline />
-        </>
+        <JournalLayout>
+            <Typography>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Culpa facilis consequatur et maxime earum itaque odit illo blanditiis reiciendis saepe, aspernatur reprehenderit odio unde, quod, pariatur esse. At, cum minus.</Typography>
+
+        </JournalLayout>
     )
 }
