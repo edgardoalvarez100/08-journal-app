d6459feb98618c911d0933c2b48bab1b64a3c59f
Edgardo Alvarez <edgardo@livedesign.org>
2023-09-04 07:28:09 -0500

    “update”

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,14 +1,25 @@
-import { Typography } from "@mui/material"
-import { MailOutline } from "@mui/icons-material"
+import { IconButton } from "@mui/material"
+import { AddOutlined } from "@mui/icons-material"
 import { JournalLayout } from "../layout/JournalLayout"
 import { NoteView, NothingSelectedView } from "../views"
 
 export const JournalPage = () => {
     return (
         <JournalLayout>
-            {/* <Typography>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Culpa facilis consequatur et maxime earum itaque odit illo blanditiis reiciendis saepe, aspernatur reprehenderit odio unde, quod, pariatur esse. At, cum minus.</Typography> */}
-            {/* <NothingSelectedView /> */}
-            <NoteView />
+            <NothingSelectedView />
+            {/* <NoteView /> */}
+            <IconButton size="large"
+                sx={{
+                    color: 'white',
+                    backgroundColor: 'error.main',
+                    ':hover': { backgroundColor: 'error.main', opacity: 0.9 },
+                    position: 'fixed',
+                    right: 50,
+                    bottom: 50
+                }}
+            >
+                <AddOutlined sx={{ fontSize: 30 }} />
+            </IconButton>
         </JournalLayout>
     )
 }
